1a94df239877b75c179982d8df89c990ff3fff27
Tonny G <118627853+tonnygitonga@users.noreply.github.com>
2023-07-04 23:51:00 +0300

    add solution 1

---
+++ Problem 1/Solution 1.pl
@@ -0,0 +1,14 @@
+doctor(simon).
+works_at(simon, a_busy_hospital).
+specializes_in(simon, cardiology).
+is_known_for(simon, expertise_in_treating_heart_diseases).
+dedicated_to(simon, profession).
+spends_long_hours_at(simon, hospital).
+ensures_well_being_of_patients(simon).
+trusts(patient,simon).
+rely_on_knowledge_and_skills(simon).
+believes_in(simon, holistic_approach_to_healthcare).
+emphasizes_importance_of(simon, medical_treatments).
+emphasizes_importance_of(simon, lifestyle_modifications).
+educates_and_supports(simon,patients).
+patient_of(X, simon) :- doctor(simon), trusts(X, simon).
