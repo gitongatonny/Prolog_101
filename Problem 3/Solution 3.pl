92c6a19b9f6940cfc838f2da6845c9e917a95a9f
Tonny G <118627853+tonnygitonga@users.noreply.github.com>
2023-07-11 08:55:03 +0300

    add siblings' rules

--- Problem 3/Solution 3.pl
+++ Problem 3/Solution 3.pl
@@ -23,3 +23,8 @@ parent(lisa, emily).
 %ParentsRules
 father(X, Y) :- male(X), parent(X, Y).
 mother(X, Y) :- female(X), parent(X, Y).
+
+%SiblingsRules
+sibling(X, Y) :- parent(Z, X), parent(Z, Y), X \= Y.
+brother(X, Y) :- male(X), sibling(X, Y).
+sister(X, Y) :- female(X), sibling(X, Y).
