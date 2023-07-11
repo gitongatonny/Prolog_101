eb9d5a636e9b99f9937d49b7175a7bc84e7dc931
Tonny G <118627853+tonnygitonga@users.noreply.github.com>
2023-07-11 08:55:37 +0300

    add grandparents' rules

--- Problem 3/Solution 3.pl
+++ Problem 3/Solution 3.pl
@@ -28,3 +28,8 @@ mother(X, Y) :- female(X), parent(X, Y).
 sibling(X, Y) :- parent(Z, X), parent(Z, Y), X \= Y.
 brother(X, Y) :- male(X), sibling(X, Y).
 sister(X, Y) :- female(X), sibling(X, Y).
+
+%GrandparentsRules
+grandparent(X, Y) :- parent(X, Z), parent(Z, Y).
+grandfather(X, Y) :- male(X), grandparent(X, Y).
+grandmother(X, Y) :- female(X), grandparent(X, Y).
