a965e228fc102578af8485c581ac8ff509906715
Tonny G <118627853+tonnygitonga@users.noreply.github.com>
2023-07-11 08:37:31 +0300

    add parents' facts to solution 3

--- Problem 3/Solution 3.pl
+++ Problem 3/Solution 3.pl
@@ -9,3 +9,13 @@ female(mary).
 female(lisa).
 female(jane).
 female(emily).
+
+%ParentsFacts
+parent(john, peter).
+parent(john, lisa).
+parent(mary, peter).
+parent(mary, lisa).
+parent(peter, david).
+parent(peter, jane).
+parent(lisa, mark).
+parent(lisa, emily).
